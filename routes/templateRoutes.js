32d16e80d82318bda63b229a45a4065e097c7759
codadhyay <codadhyay@gmail.com>
2025-09-22 12:35:57 +0530

    Added one more filter for templates

--- routes/templateRoutes.js
+++ routes/templateRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Template = require('../models/Template');
+const Template = require('../models/Templates');
 
 // Get all templates with filters
 // GET /api/templates?category=THOUGHTS
 router.get('/', async (req, res) => {
   try {
-    const { category, sub_category } = req.query;
+    const { category, sub_category, paid } = req.query;
 
     let filter = {};
 
+    if (paid) filter.paid = paid;
     if (category) filter.category = category;
     if (sub_category) filter.sub_category = sub_category;
 
@@ -19,3 +20,5 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+module.exports = router;
