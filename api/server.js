ec09d156dca1e262ca26db6a08b48441ce6b9dfc
codadhyay <codadhyay@gmail.com>
2025-09-22 12:15:02 +0530

    Handled moment package

--- api/server.js
+++ api/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config();
 const app = express();
 
 app.use(express.json());
