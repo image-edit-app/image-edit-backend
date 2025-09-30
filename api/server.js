e1fb9e64b2edb49a419ccb89b1e3554c3410f388
codadhyay <codadhyay@gmail.com>
2025-09-30 15:12:47 +0530

    Increased limit

--- api/server.js
+++ api/server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
 
 // Import routes
